cf88c1270e31e82dd33e0d8eee47b36c1e6f88f1
jhrichards10 <114703580+jhrichards10@users.noreply.github.com>
2022-11-03 22:48:59 +0000

    fixed bugs and syntax

--- main.py
+++ main.py
@@ -8,84 +8,95 @@ def readbook():
 #### Should I use functions or just create loops ?????
 # Create variable to hold Dracula text
 draculaText = readbook()
+# Split 
+draculaText = draculaText.split()
 
 # Create an empty dictionary
 draculaWords = {}
 
-# Create count for most word
-often = 0
 # Create a for loop for most often
-for word in readbook():
+for word in draculaText:
+  if(word in draculaWords):
+    draculaWords[word] += 1
+  else:
+    draculaWords[word] = 1
+
+highRank = 0
+word = ""
+fourCount = 0
+for key, value in draculaWords.items():
+  if value > highRank:
+    highRank = value
+    word = key
+  if len(key) == 4:
+    fourCount += 1 
 
   # make all words lowercase 
-  if(word.lower() in draculaText):
+ # if(word.lower() in draculaText):
 
-    # finish the count
-    often += 1
 
 # Create function for word that appears most often 
-def mostOften(word, often):
+#def mostOften(word, often):
 
   # Create for loop to find word in dracula text
-  for word in readbook():
+ # for word in readbook():
 
     # make all words lowercase
-    word = word.lower()
+  #  word = word.lower()
 
 # Create for loop to count four letter words 
-def fourLetterWord():
-  appearances = 0
-  for fourLetter in readbook():
-    # make all words lowercase
-    fourLetter = fourLetter.lower()
+# def fourLetterWord():
+#   appearances = 0
+#   for fourLetter in readbook():
+#     # make all words lowercase
+#     fourLetter = fourLetter.lower()
   
-    # if statement to count length
-    if len(fourLetter) == 4:
+#     # if statement to count length
+#     if len(fourLetter) == 4:
     
-      # finish the count starte earlier
-      appearances += 1 
+#       # finish the count starte earlier
+#       appearances += 1 
   
-  # Create a print to print out how many four letter words there are
-  print(f"There are {appearances} words that are four letters long")
+#   # Create a print to print out how many four letter words there are
+#   print(f"There are {appearances} words that are four letters long")
 
-# Start the count =
-count = 0
+# # Start the count =
+# count = 0
 
-# Create a for loop to count the words that show up at least 500 times 
-for words in readbook():
+# # Create a for loop to count the words that show up at least 500 times 
+# for words in readbook():
 
-  # if word appears more than 500 times
-  if(count >= 500):
+#   # if word appears more than 500 times
+#   if(count >= 500):
 
-    # count the number of times
-    count += 1
+#     # count the number of times
+#     count += 1
 
-    # count in the dictionary created at the beginning
-    draculaWords[words] = count 
+#     # count in the dictionary created at the beginning
+#     draculaWords[words] = count 
 
-# Create the print functions
-print("=== Results ===")
+# # Create the print functions
+# print("=== Results ===")
 
-#Space
-print()
+# #Space
+# print()
 
-# Call the word that appears the most often 
-mostOften(often, word)
-print(f"{word} is the word that appears the most through the text, a total of {often} times ")
+# # Call the word that appears the most often 
+# mostOften(often, word)
+print(f"'{word}' is the word that appears the most through the text, a total of {highRank} times ")
 
 # Space
 print()
 
 # Call the amount of 4 letter words in dracula
-fourLetterWord()
-#print(f"There are {fourLetterWord} words that are four letters long ")
+print(f"There are {fourCount} words that are four letters long ")
 
 # Space
 print()
 
-# Call the dictionary for words in dracula 500 times
+# Set up the 500 or more words
+print("I noticed that these words show up 500 or more times:")
+# # Call the dictionary for words in dracula 500 times
 for key, value in draculaWords.items():
-
-  # print the key value of dictionary, #### Not Working #### Do not know why
-  print(f"{key} - {value}")
-
+  if value >= 500:
+    print(f"{key} - {value}")
